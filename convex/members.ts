6fc2685e0339e818cdb178bc759e90125302a48d
Shubham996633 <risjiomaujio@gmai;.com>
2024-09-13 20:36:20 +0530

    feat: workspace-sidebar complete

--- convex/members.ts
+++ convex/members.ts
@@ -1,6 +1,11 @@
 import { v } from "convex/values";
-import { query } from "./_generated/server";
+import { Id } from "./_generated/dataModel";
+import { query, QueryCtx } from "./_generated/server";
 import { auth } from "./auth";
+
+const populateUser = (ctx: QueryCtx, id: Id<"users">) => {
+  return ctx.db.get(id);
+};
 
 export const current = query({
   args: { workspaceId: v.id("workspaces") },
@@ -18,5 +23,39 @@ export const current = query({
     if (!memeber) return null;
 
     return memeber;
+  },
+});
+
+export const get = query({
+  args: { workspaceId: v.id("workspaces") },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
+    if (!userId) return [];
+
+    const memeber = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.workspaceId).eq("userId", userId)
+      )
+      .unique();
+
+    if (!memeber) return [];
+
+    const data = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id", (q) =>
+        q.eq("workspaceId", args.workspaceId)
+      )
+      .collect();
+
+    const members = [];
+    for (const member of data) {
+      const user = await populateUser(ctx, member.userId);
+      if (user) {
+        members.push({ ...member, user });
+      }
+    }
+
+    return members;
   },
 });
